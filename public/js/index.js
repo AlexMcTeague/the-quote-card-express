2a2af054e2851d3ad177ecc6c155d76926f3461d
Flarkinater <mcteaguealex@gmail.com>
2026-06-25 17:25:59 -0400

    Add quote looping functionality

---
+++ public/js/index.js
@@ -0,0 +1,40 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+const quotes = [
+    {
+        quote: "All hands! Abandon ship!",
+        author: "Captain Picard",
+    },
+    {
+        quote: "Doh!",
+        author: "Homer Simpson",
+    },
+    {
+        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+        author: "Eric Schmidt",
+    },
+    {
+        quote: "Limitations foster creativity. Tell an artist to paint anything, and he may struggle, but tell him to create something specific, in a set amount of time, for a certain audience, and these constraints might well push him to produce something he might never have come up with on his own.",
+        author: "J.C. McCrae",
+    }
+]
+
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
+
+setTimeout(loopThroughQuotes, 3000);
