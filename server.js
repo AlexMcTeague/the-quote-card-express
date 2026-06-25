134976e5f8540bec7317a5d7639c529d8a3bb381
Flarkinater <mcteaguealex@gmail.com>
2026-06-25 19:58:47 -0400

    Install dotenv and cors packages

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 "use strict";
 
+require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 8081;
 
+app.use(cors());
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
